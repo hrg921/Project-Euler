126c7cdefcf013c79c9179fd81046f9227383e94
Tony <hrg921@gmail.com>
2020-12-13 20:01:47 +0900

    solved Problem 1

---
+++ 1/solution.ts
@@ -0,0 +1,14 @@
+const number = 1000;
+
+(() => {
+  let sum = 0;
+
+  for (let i = 1; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      console.log(i);
+      sum += i;
+    }
+  }
+
+  console.log(sum);
+})();
